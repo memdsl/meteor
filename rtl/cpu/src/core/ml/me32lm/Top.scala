2786ea2a1a01eb7ef0f996683f2d08f7fbe11b09
myyerrol <myyerrol@126.com>
2024-01-25 07:05:30 +0800

    chore: add empty core files

---
+++ rtl/cpu/src/core/ml/me32lm/Top.scala
@@ -0,0 +1,7 @@
+package cpu.core.ml.me32lm
+
+import chisel3._
+import chisel3.util._
+
+class Top extends Module {
+}
---
+++ rtl/cpu/src/core/ml/me32lp/Top.scala
@@ -0,0 +1,7 @@
+package cpu.core.ml.me32lp
+
+import chisel3._
+import chisel3.util._
+
+class Top extends Module {
+}
