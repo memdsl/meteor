a6beb3d036694f279caab62df0f314d1009f3747
myyerrol <myyerrol@126.com>
2024-01-28 11:31:56 +0800

    feat: update mem module

--- rtl/cpu/src/core/ml/me32ls/Top.scala
+++ rtl/cpu/src/core/ml/me32ls/Top.scala
@@ -3,6 +3,8 @@ package cpu.core.ml.me32ls
 import chisel3._
 import chisel3.util._
 
+import cpu.mem._
+
 class Top extends Module {
     val io = IO(new Bundle {
         val a = Input(UInt(32.W))
@@ -10,5 +12,20 @@ class Top extends Module {
         val c = Output(UInt(32.W))
     });
 
-    io.c := io.a + io.b;
+    val mMemDualFake = Module(new MemDualFake("sync"))
+    mMemDualFake.io.iRdEn := true.B
+    mMemDualFake.io.iWrEn := true.B
+
+    mMemDualFake.io.iRdAddr := 0.U
+    mMemDualFake.io.iWrAddr := 0.U
+    mMemDualFake.io.iWrData := 0.U
+
+    val maskInt = "b1110".U
+    val maskVec = VecInit(maskInt.asBools)
+    mMemDualFake.io.iWrMask := maskVec
+
+    val test = WireInit(UInt(32.W), 0.U)
+    test := mMemDualFake.io.oRdData
+
+    io.c := 1.U + test
 }
--- rtl/cpu/src/mem/MemDualFake.scala
+++ rtl/cpu/src/mem/MemDualFake.scala
@@ -13,25 +13,39 @@ class MemDualFake(val cTimeType: String) extends Module with ConfigInst {
         val iWrEn   = Input(Bool())
         val iWrAddr = Input(UInt(ADDR_WIDTH.W))
         val iWrData = Input(UInt(DATA_WIDTH.W))
-        val iWrMask = Input(UInt(BYTE_WIDTH.W))
+        val iWrMask = Input(Vec(MASK_WIDTH, Bool()))
 
         val oRdData = Output(UInt(DATA_WIDTH.W))
     })
 
     val mMem = cTimeType match {
-        case "async" => Mem(MEMS_NUM, UInt(DATA_WIDTH.W))
-        case "sync"  => SyncReadMem(MEMS_NUM, UInt(DATA_WIDTH.W))
+        case "async" => Mem(MEMS_NUM,
+                            Vec(MASK_WIDTH, UInt(BYTE_WIDTH.W)))
+        case "sync"  => SyncReadMem(MEMS_NUM,
+                                    Vec(MASK_WIDTH, UInt(BYTE_WIDTH.W)))
     }
 
-    io.oRdData := (mMem match {
+    val wRdData = Wire(Vec(MASK_WIDTH, UInt(BYTE_WIDTH.W)))
+
+    wRdData := (mMem match {
         case asyncMem: Mem[_]         => asyncMem.read(io.iRdAddr)
-        case syncMem:  SyncReadMem[_] => syncMem.read(io.iRdAddr)
+        case syncMem:  SyncReadMem[_] => syncMem.read(io.iRdAddr, io.iRdEn)
     })
 
+    io.oRdData := wRdData.reverse.foldLeft(0.U(BYTE_WIDTH.W)) {
+        (sum, nxt) => Cat(nxt, sum)
+    }
+
+    val wWrData = Wire(Vec(MASK_WIDTH, UInt(BYTE_WIDTH.W)))
+    for (i <- 0 until MASK_WIDTH) {
+        wWrData(i) := io.iWrData(BYTE_WIDTH * i + (BYTE_WIDTH - 1),
+                                 BYTE_WIDTH * i)
+    }
+
     when (io.iWrEn) {
-        mMem(io.iWrAddr) := io.iWrData
+        mMem.write(io.iWrAddr, wWrData, io.iWrMask)
     }
     .otherwise {
-        mMem(io.iWrAddr) := mMem(io.iWrAddr)
+        mMem.write(io.iWrAddr, mMem.read(io.iWrAddr), io.iWrMask)
     }
 }
