5c8bea641820e5a27b6de94b93323e5429633bf6
myyerrol <myyerrol@126.com>
2024-02-12 09:03:39 +0800

    style: rename dpi-c functions

--- rtl/cpu/srcv/mem/MemDualFakeBB.v
+++ rtl/cpu/srcv/mem/MemDualFakeBB.v
@@ -16,25 +16,26 @@ module MemDualFakeBB(
     output reg  [`DATA_WIDTH - 1 : 0] pMem_bRdDataB
 );
 
-    import "DPI-C" context function int unsigned readInsData(
+    import "DPI-C" context function int unsigned readSimInstData(
         input int unsigned addr,
         input byte unsigned len);
-    import "DPI-C" context function int unsigned readMemData(
+    import "DPI-C" context function int unsigned readSimMemoryData(
         input int unsigned addr,
         input byte unsigned len);
-    import "DPI-C" context function void writeMemData(input int unsigned addr,
-                                                      input int unsigned data,
-                                                      input byte unsigned len);
+    import "DPI-C" context function void writeSimMemoryData(
+        input int unsigned addr,
+        input int unsigned data,
+        input byte unsigned len);
 
     always @(pMem_bRdAddrA) begin
         if (pMem_bRdEn) begin
-            pMem_bRdDataA = readInsData(pMem_bRdAddrA, 4);
+            pMem_bRdDataA = readSimInstData(pMem_bRdAddrA, 4);
         end
     end
 
     always @(pMem_bRdAddrB) begin
         if (pMem_bRdEn) begin
-            pMem_bRdDataB = readMemData(pMem_bRdAddrB, 4);
+            pMem_bRdDataB = readSimMemoryData(pMem_bRdAddrB, 4);
         end
     end
 
@@ -45,10 +46,10 @@ module MemDualFakeBB(
     always @(pMem_bWrAddr or pMem_bWrData) begin
         if (pMem_bWrEn) begin
             case (pMem_bWrMask)
-                4'b0001: writeMemData(pMem_bWrAddr, pMem_bWrData, 1);
-                4'b0011: writeMemData(pMem_bWrAddr, pMem_bWrData, 2);
-                4'b1111: writeMemData(pMem_bWrAddr, pMem_bWrData, 4);
-                default: writeMemData(pMem_bWrAddr, pMem_bWrData, 4);
+                4'b0001: writeSimMemoryData(pMem_bWrAddr, pMem_bWrData, 1);
+                4'b0011: writeSimMemoryData(pMem_bWrAddr, pMem_bWrData, 2);
+                4'b1111: writeSimMemoryData(pMem_bWrAddr, pMem_bWrData, 4);
+                default: writeSimMemoryData(pMem_bWrAddr, pMem_bWrData, 4);
             endcase
         end
     end
