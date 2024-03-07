22bb57fd1fbef53afc918222ce256f22f0dee6f3
myyerrol <myyerrol@126.com>
2024-03-07 19:01:06 +0800

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+PATH_ROOT=${PWD}
+PATH_ENV=${PATH_ROOT}/env/
+PATH_IP=${PATH_ROOT}/ip/
+
+if [ ! -d ${PATH_ENV} ]; then
+    echo "Downloading env..."
+    rm -rf ${PATH_ENV}
+    git clone git@github.com:myyerrol/blackhole.git ${PATH_ENV}
+else
+    echo "Updating common scripts..."
+    cd ${PATH_ENV}
+    git pull origin main
+    cd ${PATH_ROOT}
+fi
+
+if [ ! -d ${PATH_IP} ]; then
+    echo ""
+    echo "Downloading ip..."
+    rm -rf ${PATH_IP}
+    git clone git@github.com:myyerrol/aurora.git ${PATH_IP}
+else
+    echo ""
+    echo "Updating ip..."
+    cd ${PATH_IP}
+    git pull origin main
+    cd ${PATH_ROOT}
+fi
