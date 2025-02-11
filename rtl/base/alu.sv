67074299799c32504a59cdd67f3d032b56ef3392
myyerrol <myyerrol@126.com>
2025-02-11 08:50:23 +0800

    fix: fix jalr bug

--- rtl/base/alu.sv
+++ rtl/base/alu.sv
@@ -12,43 +12,30 @@ module alu(
     output logic                       o_alu_neg
 );
 
-    logic [`DATA_WIDTH - 1 : 0] w_mask;
-
-    assign w_mask = {{(`DATA_WIDTH - 1){1'b1}}, 1'b0};
-
-    logic [`DATA_WIDTH - 1 : 0] w_u_rs1_data;
-    logic [`DATA_WIDTH - 1 : 0] w_u_rs2_data;
-    logic [`DATA_WIDTH - 1 : 0] w_s_rs1_data;
-    logic [`DATA_WIDTH - 1 : 0] w_s_rs2_data;
-    logic [              5 : 0] w_u_rs2_data_shift;
-
-    assign w_u_rs1_data       = i_alu_rs1_data;
-    assign w_u_rs2_data       = i_alu_rs2_data;
-    assign w_s_rs1_data       = $signed(i_alu_rs1_data);
-    assign w_s_rs2_data       = $signed(i_alu_rs2_data);
-    assign w_u_rs2_data_shift = (`DATA_WIDTH === 32 ?
-                                {1'b0, w_u_rs2_data[4 : 0]} :
-                                       w_u_rs2_data[5 : 0]);
+    logic [5 : 0] w_alu_rs2_data_shift;
+    assign w_alu_rs2_data_shift = `DATA_WIDTH === 32 ?
+                                  {1'b0, i_alu_rs2_data[4 : 0]} :
+                                         i_alu_rs2_data[5 : 0];
 
     always_comb begin
         case (i_alu_type)
-            `ALU_TYPE_SLL : o_alu_res = w_u_rs1_data <<  w_u_rs2_data_shift;
-            `ALU_TYPE_SRL : o_alu_res = w_u_rs1_data >>  w_u_rs2_data_shift;
-            `ALU_TYPE_SRA : o_alu_res = w_u_rs1_data >>> w_u_rs2_data_shift;
-            `ALU_TYPE_ADD : o_alu_res = w_u_rs1_data +   w_u_rs2_data;
-            `ALU_TYPE_SUB : o_alu_res = w_u_rs1_data -   w_u_rs2_data;
-            `ALU_TYPE_XOR : o_alu_res = w_u_rs1_data ^   w_u_rs2_data;
-            `ALU_TYPE_OR  : o_alu_res = w_u_rs1_data |   w_u_rs2_data;
-            `ALU_TYPE_AND : o_alu_res = w_u_rs1_data &   w_u_rs2_data;
+            `ALU_TYPE_SLL : o_alu_res = i_alu_rs1_data          <<  w_alu_rs2_data_shift;
+            `ALU_TYPE_SRL : o_alu_res = i_alu_rs1_data          >>  w_alu_rs2_data_shift;
+            `ALU_TYPE_SRA : o_alu_res = $signed(i_alu_rs1_data) >>> w_alu_rs2_data_shift;
+            `ALU_TYPE_ADD : o_alu_res =         i_alu_rs1_data  +   i_alu_rs2_data;
+            `ALU_TYPE_SUB : o_alu_res =         i_alu_rs1_data  -   i_alu_rs2_data;
+            `ALU_TYPE_XOR : o_alu_res =         i_alu_rs1_data  ^   i_alu_rs2_data;
+            `ALU_TYPE_OR  : o_alu_res =         i_alu_rs1_data  |   i_alu_rs2_data;
+            `ALU_TYPE_AND : o_alu_res =         i_alu_rs1_data  &   i_alu_rs2_data;
             `ALU_TYPE_SLT : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, ($signed(i_alu_rs1_data) <   $signed(i_alu_rs2_data))};
-            `ALU_TYPE_SLTU: o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (w_u_rs1_data <   w_u_rs2_data)};
-            `ALU_TYPE_BEQ : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (w_u_rs1_data === w_u_rs2_data)};
-            `ALU_TYPE_BNE : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (w_u_rs1_data !== w_u_rs2_data)};
+            `ALU_TYPE_SLTU: o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (i_alu_rs1_data <   i_alu_rs2_data)};
+            `ALU_TYPE_BEQ : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (i_alu_rs1_data === i_alu_rs2_data)};
+            `ALU_TYPE_BNE : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (i_alu_rs1_data !== i_alu_rs2_data)};
             `ALU_TYPE_BLT : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, ($signed(i_alu_rs1_data) <   $signed(i_alu_rs2_data))};
             `ALU_TYPE_BGE : o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, ($signed(i_alu_rs1_data) >=  $signed(i_alu_rs2_data))};
-            `ALU_TYPE_BLTU: o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (w_u_rs1_data <   w_u_rs2_data)};
-            `ALU_TYPE_BGEU: o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (w_u_rs1_data >=  w_u_rs2_data)};
-            `ALU_TYPE_JALR: o_alu_res = (w_u_rs1_data +  w_u_rs2_data) + w_mask;
+            `ALU_TYPE_BLTU: o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (i_alu_rs1_data <   i_alu_rs2_data)};
+            `ALU_TYPE_BGEU: o_alu_res = {{(`DATA_WIDTH - 1){1'b0}}, (i_alu_rs1_data >=  i_alu_rs2_data)};
+            `ALU_TYPE_JALR: o_alu_res = (i_alu_rs1_data +  i_alu_rs2_data) & {{(`DATA_WIDTH - 1){1'b1}}, 1'b0};
             default       : o_alu_res = `DATA_ZERO;
         endcase
     end
